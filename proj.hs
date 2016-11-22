7c0f5ebddb58380b165aed319898f4c5b383354e
Nono-G <noe.goudian@etu.univ-amu.fr>
2016-11-22 09:54:02 +0100

    Noé : encodage Ok

--- proj.hs
+++ proj.hs
@@ -10,13 +10,36 @@ class Table a where
 
 data AssoList = C (String,Code) AssoList | Vide
 
+alV :: AssoList
+alV = Vide
+al1 :: AssoList
+al1 = C ("a",0) (C ("b",1) (C ("ab",2) (C ("c",3) (C ("bc",4) Vide))))
+--al2 :: AssoList
+--al2 C () (C () (C () ()))
+
 alLast :: AssoList -> (String,Code)
 alLast (C paire Vide) = paire
 alLast (C paire suite) = alLast suite
 
+bodyShow :: AssoList -> String
+bodyShow Vide = ""
+bodyShow (C (x,s) Vide) = "("++(show x)++","++(show s)++")"
+bodyShow (C (x,s) reste) = "("++(show x)++","++(show s)++"):"++(bodyShow reste)
 instance Show AssoList where
-	show Vide = "[]"
-	show (C (x,s) reste) = "("++(show x)++","++(show s)++"):"++(show reste)
+	show al = "["++(bodyShow al)++"]";
+
+prefixeAcc :: String -> String -> String -> String
+prefixeAcc acc [] ys = acc
+prefixeAcc acc xs [] = acc
+prefixeAcc acc (x:xs) (y:ys) = if x == y
+				then prefixeAcc (acc++[x]) xs ys
+				else acc
+
+plusLongPrefixeAcc :: String -> String -> AssoList -> String
+plusLongPrefixeAcc acc mot Vide = acc
+plusLongPrefixeAcc acc mot (C (s,c) reste) = if (length acc) < (length (prefixeAcc "" mot s))
+						then plusLongPrefixeAcc s mot reste
+						else plusLongPrefixeAcc acc mot reste
 
 instance Table AssoList where
 	empty = Vide
@@ -41,8 +64,8 @@ instance Table AssoList where
 					else (isIn reste x) 
 	--
 	split Vide x = ("", Nothing, x)
-	split 
-	split _ _ = ("aaa", Nothing, "bbb")
+	split al mot = let pref = (plusLongPrefixeAcc "" mot al)
+			 in (pref, (codeOf al pref), (drop (length pref) mot))
 	--
 
 contains :: Eq a => [a] -> a -> Bool
@@ -55,13 +78,32 @@ charsInAcc (x:xs) ys = if ( contains ys x)
 			then charsInAcc xs ys
 			else charsInAcc xs (ys ++ [x])
 
---initTable :: String -> Int -> Table
+initTable0 :: String -> AssoList
+initTable0 [] = Vide
+initTable0 (x:xs) = insert (initTable0 xs) [x]
 
+initTable :: String -> AssoList
+initTable s = initTable0 (reverse (charsInAcc s ""))
 
-charsIn :: String -> String
-charsIn xs = charsInAcc xs []
+fromJust :: Maybe a -> a
+fromJust (Just x) = x
 
---lzwEncode :: Table a => a -> String -> [Code]
+lzwEncode0 :: Table a => a -> String -> [Code]
+lzwEncode0 al texte = let (pref, mc, suff) = split al texte
+			in if suff == []
+				then [(fromJust mc)]
+				else ((fromJust mc):(lzwEncode0 (insert al (pref++[(head suff)])) suff))
 
+lzwEncode0D :: Table a => a -> String -> ([Code],a)
+lzwEncode0D al texte = let (pref, mc, suff) = split al texte
+			in if suff == []
+				then ([(fromJust mc)],al)
+				else let (cs, t) = (lzwEncode0D (insert al (pref++[(head suff)])) suff)
+					in (((fromJust mc):cs),t)	
+lzwEncodeA :: String -> [Code]
+lzwEncodeA str = lzwEncode0 (initTable str) str
+
+lzwEncodeAD :: String -> ([Code],AssoList)
+lzwEncodeAD str = lzwEncode0D (initTable str) str
 
 --lzwDecode :: Table a => a -> [Code] -> String
